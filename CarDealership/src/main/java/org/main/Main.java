ea1890a140dd0e384157a40a6838032eafa08e3e
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-11-06 14:07:18 -0500

    Phase 4

---
+++ CarDealership/src/main/java/org/main/Main.java
@@ -0,0 +1,11 @@
+package org.main;
+
+import org.ui.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+}
