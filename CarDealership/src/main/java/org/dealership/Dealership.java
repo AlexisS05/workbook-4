2d6fdee7141413d96b2db2fde08be0c5b9a330d3
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-11-06 10:54:59 -0500

    Dealership.java before Phase 3

--- CarDealership/src/main/java/org/dealership/Dealership.java
+++ CarDealership/src/main/java/org/dealership/Dealership.java
@@ -18,42 +18,85 @@ public class Dealership {
         inventory = new ArrayList<>();
     }
 
-    public List<Vehicle> getVehiclesByPrice(double min, double max) {
-        return null;
-    }
-
-    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
-        return null;
-    }
-
-    public List<Vehicle> getVehiclesByYear(double min, double max) {
-        return null;
-    }
-
-    public List<Vehicle> getVehiclesByColor(String color) {
-        return null;
-    }
-
-    public List<Vehicle> getVehiclesByMileage(double min, double max) {
-        return null;
-    }
-
-    public List<Vehicle> getVehiclesByType(String vehicleType) {
-        return null;
-    }
-
-    public List<Vehicle> getAllVehicles() {
+    public ArrayList<Vehicle> getInventory() {
         return inventory;
     }
 
-    public void addVehicle(Vehicle vehicle) {
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+        ArrayList<Vehicle> inventoryWithinRange = new ArrayList<>();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getPrice() >= min && vehicle.getPrice() <= max){
+                inventoryWithinRange.add(vehicle);
+            }
+        }
+        return inventoryWithinRange;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+        ArrayList<Vehicle> inventoryWithinMakeModel = new ArrayList<>();
+        String makeParsed = make.trim();
+        String modelParsed = model.trim();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getMake().equalsIgnoreCase(makeParsed) && vehicle.getModel().equalsIgnoreCase(modelParsed)){
+                inventoryWithinMakeModel.add(vehicle);
+            }
+        }
+        return inventoryWithinMakeModel;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        ArrayList<Vehicle> inventoryWithinRange = new ArrayList<>();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getYear() >= min && vehicle.getYear() <= max){
+                inventoryWithinRange.add(vehicle);
+            }
+        }
+        return inventoryWithinRange;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color){
+        ArrayList<Vehicle> inventoryWithinColor = new ArrayList<>();
+        String makeParsed = color.trim();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getColor().equalsIgnoreCase(color)){
+                inventoryWithinColor.add(vehicle);
+            }
+        }
+        return inventoryWithinColor;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max){
+        ArrayList<Vehicle> inventoryWithinRange = new ArrayList<>();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getOdometer() >= min && vehicle.getOdometer() <= max){
+                inventoryWithinRange.add(vehicle);
+            }
+        }
+        return inventoryWithinRange;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String type){
+        ArrayList<Vehicle> inventoryWithinType = new ArrayList<>();
+        String makeParsed = type.trim();
+        for(Vehicle vehicle: inventory){
+            if(vehicle.getColor().equalsIgnoreCase(type)){
+                inventoryWithinType.add(vehicle);
+            }
+        }
+        return inventoryWithinType;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles(){
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle){
         inventory.add(vehicle);
     }
 
-    public void removeVehicle(Vehicle vehicle) {
-
+    public void removeVehicle(Vehicle vehicle){
+        inventory.remove(vehicle);
     }
-
     @Override
     public String toString() {
         return "Dealership{" +
