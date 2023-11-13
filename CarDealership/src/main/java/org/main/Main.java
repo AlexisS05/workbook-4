8bf38e7139438aaeaf5e1d7100e94f5b1e558d3d
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-11-12 19:21:17 -0500

    Added main

--- CarDealership/src/main/java/org/main/Main.java
+++ CarDealership/src/main/java/org/main/Main.java
@@ -4,7 +4,6 @@ import org.ui.UserInterface;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         UserInterface ui = new UserInterface();
         ui.display();
     }
