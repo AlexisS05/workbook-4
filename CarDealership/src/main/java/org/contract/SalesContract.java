5eed8f4bf544b3897a370eeddfbb45de8215b2d1
Alexis Sanchez <sanchezalexis500@gmail.com>
2023-11-14 00:21:41 -0500

    added String for SalesContract

--- CarDealership/src/main/java/org/contract/SalesContract.java
+++ CarDealership/src/main/java/org/contract/SalesContract.java
@@ -2,9 +2,17 @@ package org.contract;
 
 import org.vehicle.Vehicle;
 
-public class SalesContract extends Contract{
-    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, double totalPrice, double monthlyPayment) {
-        super(date, customerName, customerEmail, vehicleSold, totalPrice, monthlyPayment);
+public class SalesContract extends Contract {
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+    }
+
+    public double getTax() {
+        return 0.05;
+    }
+
+    public double getProcessingFee() {
+        return 100;
     }
 
     @Override
@@ -15,5 +23,24 @@ public class SalesContract extends Contract{
     @Override
     public double getMonthlyPayment() {
         return 0;
+    }
+
+    @Override
+    public String getContractString() {
+        return String.format("SALE|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s \n",
+                getDate(),
+                getCustomerName(),
+                getCustomerEmail(),
+                getVehicleSold().getVin(),
+                getVehicleSold().getYear(),
+                getVehicleSold().getMake(),
+                getVehicleSold().getModel(),
+                getVehicleSold().getVehicleType(),
+                getVehicleSold().getColor(),
+                getVehicleSold().getOdometer(),
+                getVehicleSold().getPrice()
+                // totalPrice?
+                // monthlyPayment?
+        );
     }
 }
